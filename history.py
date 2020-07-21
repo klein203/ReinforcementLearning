9b57ea120f0bfdb17bf2d6b229d680478ff587e4
xusheng <klein203@gmail.com>
2020-07-21 20:44:39 +0800

    simple history for params tracing

---
+++ history.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+class History(object):
+    def __init__(self):
+        self.m = dict()
+
+    def store(self, episode, prop, val):
+        key = str(prop)
+        if key not in self.m:
+            self.m[key] = np.array(val.reshape(1, -1))
+        else:
+            self.m[key] = np.insert(self.m.get(key), -1, values=val, axis=0)
+
+    def get(self, prop):
+        key = str(prop)
+        return self.m.get(key)
+
+
+if __name__ == "__main__":
+    h = History()
+    h.store(1, 'test', np.random.standard_normal(5))
+    h.store(1, 'test', np.random.standard_normal(5))
+    h.store(1, 'test', np.random.standard_normal(5))
+    print(h.get('test'))
