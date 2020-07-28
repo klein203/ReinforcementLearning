softmax policy exploration bug fix, run figure

bandit/env.py
@@ -37,8 +37,8 @@ class MultiArmedBanditEnv(AbstractMultiArmedBanditEnv):
     def set_reward_target_dist_mean(self, mean=0.0, var=1.0):
         self.reward_target_dist_mean = np.random.normal(loc=mean, scale=var, size=self.k)
     
-    def get_reward_target_expectation(self):
-        return self.reward_target_dist_mean
+    def get_optimal_target_action(self):
+        return np.argmax(self.reward_target_dist_mean)
 
     def get_reward(self, action_idx):
         # R(a) ~ N(q*(a), 1)

bandit/policy.py
@@ -68,5 +68,5 @@ class SoftmaxPolicy(AbstractPolicy):
     
     def choose(self, val_actions, ntimes_actions, *args, **kwargs):
         probabilities = np.exp(val_actions) / np.sum(np.exp(val_actions))
-        actual = np.argmax(probabilities)
+        actual = np.random.choice(range(len(val_actions)), p=probabilities)
         return actual, probabilities

bandit/starter.py
@@ -260,7 +260,7 @@ def ch2_8(n_episodes=1000, n_steps=1000):
     env = MultiArmedBanditEnv(actions_space)
     env.set_reward_target_dist_mean(4.0, 1.0)
     env.info()
-    optimal_action = np.argmax(env.get_reward_target_expectation())
+    optimal_action = env.get_optimal_target_action()
     logging.info('Optimal Action Index = %d' % optimal_action)
 
     # params storing
@@ -315,12 +315,12 @@ def test(n_episodes=100, n_steps=1000):
     n_arm = 10
     actions_space = ['Arm%d' % i for i in range(n_arm)]
     env = MultiArmedBanditEnv(actions_space)
-    env.set_reward_target_dist_mean(4.0, 1.0)
+    env.set_reward_target_dist_mean(0.0, 1.0)
     env.info()
-    optimal_action = np.argmax(env.get_reward_target_expectation())
+    optimal_action = env.get_optimal_target_action()
     logging.info('Optimal Action Index = %d' % optimal_action)
 
-    agent = GradientBanditAlg(env, step_size=0.1, r_baseline=4)
+    agent = GradientBanditAlg(env, step_size=0.1, r_baseline=0)
     agent.run_episodes(SoftmaxPolicy(), n_episodes, n_steps)
     agent.report()
     # actions_steps = agent.history.get('actions_steps')